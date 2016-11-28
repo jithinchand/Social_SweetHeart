90868cfd3f8b7e5d14774799d33d8f4da4b36cf3
Jithin Chand Vadlamudi <jithinchandbtech@gmail.com>
2016-11-28 21:51:42 +0100

    basic questions slide

--- poll/pollCtrl.js
+++ poll/pollCtrl.js
@@ -10,4 +10,28 @@ angular
     .controller('PollCtrl', function ($scope, pollData) {
         'use strict';
         console.log(pollData);
+        $scope.pollData = pollData.data.questions;
+        var index = 0;
+        $scope.showQuestions = true;
+        $scope.newQuestion = $scope.pollData[0].question;
+        $scope.options = $scope.pollData[0].options;
+        $scope.reward = 0;
+        $scope.totalReward = 0;
+        $scope.nextQuestion = function() {
+            $scope.totalReward += $scope.reward;
+            console.log($scope.totalReward);
+            index++;
+            if (index >= $scope.pollData.length) {
+                $scope.showQuestions = false;
+                $scope.result = ($scope.totalReward * 100)/(10 * $scope.pollData.length)
+            }
+            else {
+                $scope.newQuestion = $scope.pollData[index].question;
+                $scope.options = $scope.pollData[index].options;
+            }
+        };
+        $scope.selectedResponse = function (reward) {
+            $scope.reward = reward;
+            console.log(reward);
+        }
     });
