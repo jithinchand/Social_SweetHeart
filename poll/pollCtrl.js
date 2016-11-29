669af4f21de4aa8648410cd79eb568a949b6bd8b
Jithin Chand Vadlamudi <jithinchandbtech@gmail.com>
2016-11-29 19:49:07 +0100

    display message updated

--- poll/pollCtrl.js
+++ poll/pollCtrl.js
@@ -23,7 +23,19 @@ angular
             index++;
             if (index >= $scope.pollData.length) {
                 $scope.showQuestions = false;
-                $scope.result = ($scope.totalReward * 100)/(10 * $scope.pollData.length)
+                $scope.result = ($scope.totalReward * 100)/(10 * $scope.pollData.length);
+                if ($scope.result >= 85) {
+                    $scope.resultData = "BOOOOMM !! YOU are a SOCIAL SWEETHEART; Can't get better than this Mate !!" +
+                        "- Hurry up and apply; The World doesn't wait for you !! Some One might occupy your seat in Social Sweetheart. You can find your matching job on the link below this web page.";
+                }
+                else if ($scope.result <= 85 && $scope.result >= 65) {
+                    $scope.resultData = "Heyy, It's OHKAYY .... YOU are a Social SemiSweet Heart; You can Re-test and become SWEETER !!" +
+                        "- You may be the guy for the companies similar to Social Sweetheart, But not for the Social Sweetheart. There's a difference !!";
+                }
+                else {
+                    $scope.resultData = "SORRY DUDEEE ... You are a Social Bitter Heart; and I hope you know the reason !!" +
+                        "- The company has its offices in Dortmund and Munich. Please avoid visiting even the premises of Social sweetheart.";
+                }
             }
             else {
                 $scope.newQuestion = $scope.pollData[index].question;
